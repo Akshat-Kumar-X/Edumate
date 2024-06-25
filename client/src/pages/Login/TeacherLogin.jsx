2d0954f3d745432476126bb45ff543409900e931
Akshat Kumar <akshatkumar10203@gmail.com>
2024-06-26 01:16:30 +0530

    Changed Auth Pages

--- client/src/pages/Login/TeacherLogin.jsx
+++ client/src/pages/Login/TeacherLogin.jsx
@@ -1,10 +1,10 @@
-// TeacherLogin.jsx
 import { useState, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import toast from 'react-hot-toast';
 import { AuthContext } from '../../../HOC/AuthContext';
 import { Link } from 'react-router-dom';
+
 
 const TeacherLogin = () => {
   const [email, setEmail] = useState('');
@@ -32,41 +32,55 @@ const TeacherLogin = () => {
   };
 
   return (
-    <section className='max-w-7xl max-auto py-10'>
-      <div className='flex flex-col justify-center items-center'>
-        <div className='flex flex-col bg-slate-100 p-10 text-black rounded-md shadow-md'>
-          <h1 className='text-black text-2xl text-center font-semibold mb-5'>
-            <span className='text-blue-500 font-bold'>Teacher</span> Login
-          </h1>
-          <form onSubmit={handleSubmit} className='flex flex-col gap-5'>
-            <input
-              type='email'
-              name='email'
-              placeholder='Enter Email...'
-              onChange={(e) => setEmail(e.target.value)}
-              value={email}
-              className='py-2 px-2 shadow-md rounded-md'
-              required
-            />
-            <input
-              type='password'
-              name='password'
-              placeholder='Enter Password...'
-              onChange={(e) => setPassword(e.target.value)}
-              value={password}
-              className='py-2 px-2 shadow-md rounded-md'
-              required
-            />
-            <button type='submit' className='bg-blue-400 text-white py-1 rounded-md hover:bg-blue-500 duration-150'>
-              Submit
-            </button>
-          </form>
-          <p className='mt-3 text-gray-600'>
-            Not registered? <Link to='/teacher-register' className='text-blue-500 underline'>Sign Up</Link>
-          </p>
+    <div className='bg-gradient-to-r from-fuchsia-200 via-indigo-100 to bg-purple-300 min-h-screen flex flex-col  items-center bg-cover'>
+      <section className='w-full py-10'>
+        <div className='flex flex-col justify-center items-center h-full'>
+          <div className='flex flex-row rounded-3xl bg-white shadow-2xl'>
+            <div className='flex-1 max-w-[550px] object-cover max-md:hidden p-5 pr-0' >
+              <img src="./src/assets/images/login.png" alt="Login Banner" />
+            </div>
+            <div className='flex-1 flex items-center'>
+              <div className='flex flex-col w-full bg-white p-10 text-black rounded-md '>
+                <img src="/logo.png" alt="Logo" className='w-40 mb-5 ml-[-10px]' />
+                <h1 className='text-[#353452] text-5xl  font-semibold mb-5'>
+                  <span className='bg-gradient-to-r from-pink-400 to-rose-500 text-transparent bg-clip-text font-bold'>Teacher</span> Login
+                </h1>
+                <p className='mb-4 text-gray-500'>Connect With Students Around the World</p>
+                <form onSubmit={handleSubmit} className='flex flex-col '>
+                  <label htmlFor="email" className='text-sm text-[#353452] ms-1'>Email</label>
+                  <input
+                    type='email'
+                    name='email'
+                    placeholder='Enter Email...'
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                    className='py-3 px-2  rounded-md bg-purple-100 border-4 border-white'
+                    required
+                  />
+                  <label htmlFor="password" className='text-sm text-[#353452] ms-1 mt-5' >Password</label>
+                  <input
+                    type='password'
+                    name='password'
+                    placeholder='Enter Password...'
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                    className='py-3 px-2 rounded-md bg-purple-100 border-4 mb-5 border-white'
+                    required
+                  />
+                  
+                  <button type='submit' className='bg-gradient-to-r text-xl text-white font-bold from-fuchsia-400 to-purple-500  py-1 rounded-md  duration-150'>
+                    Submit
+                  </button>
+                </form>
+                <p className='mt-3 text-gray-600'>
+                  Not registered? <Link to='/teacher-register' className='text-blue-500 underline'>Sign Up</Link>
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 };
 
--- client/src/pages/Register/TeacherRegister.jsx
+++ client/src/pages/Register/TeacherRegister.jsx
@@ -38,72 +38,109 @@ const TeacherRegister = () => {
   };
 
   return (
-    <section className='max-w-7xl max-auto py-10'>
-      <div className='flex flex-col justify-center items-center'>
-        <div className='flex flex-col bg-slate-100 p-10 text-black rounded-md shadow-md'>
-          <h1 className='text-black text-2xl text-center font-semibold mb-5'>
-            <span className='text-blue-500 font-bold'>Teacher</span> SignUp
-          </h1>
-          <form onSubmit={handleSubmit} className='flex flex-col gap-5'>
-            <input
-              type="text"
-              name="name"
-              placeholder='Enter Name...'
-              onChange={(e) => setName(e.target.value)}
-              value={name}
-              className='py-2 px-2 shadow-md rounded-md'
-              required
-            />
-            <input
-              type="email"
-              name="email"
-              placeholder='Enter Email...'
-              onChange={(e) => setEmail(e.target.value)}
-              value={email}
-              className='py-2 px-2 shadow-md rounded-md'
-              required
-            />
-            <input
-              type="password"
-              name="password"
-              placeholder='Enter Password...'
-              onChange={(e) => setPassword(e.target.value)}
-              value={password}
-              className='py-2 px-2 shadow-md rounded-md'
-              required
-            />
-            <input
-              type="text"
-              name="subject"
-              placeholder='Enter Subject...'
-              onChange={(e) => setSubject(e.target.value)}
-              value={subject}
-              className='py-2 px-2 shadow-md rounded-md'
-            />
-            <input
-              type="number"
-              name="experience"
-              placeholder='Enter Experience (in years)...'
-              onChange={(e) => setExperience(e.target.value)}
-              value={experience}
-              className='py-2 px-2 shadow-md rounded-md'
-            />
-            <input
-              type="text"
-              name="location"
-              placeholder='Enter Location...'
-              onChange={(e) => setLocation(e.target.value)}
-              value={location}
-              className='py-2 px-2 shadow-md rounded-md'
-            />
-            <button type="submit" className='bg-blue-400 text-white py-1 rounded-md hover:bg-blue-500 duration-150'>
-              Submit
-            </button>
-          </form>
-          <p className='mt-3 text-gray-600'>Already registered? <Link to='/teacher-login' className='text-blue-500 underline'>login</Link></p>
+    <div className='bg-gradient-to-r from-fuchsia-200 via-indigo-100 to bg-purple-300 min-h-screen flex flex-col  items-center bg-cover'>
+      <section className='w-full py-10'>
+        <div className='flex flex-col justify-center items-center h-full'>
+          <div className='flex flex-row rounded-3xl bg-white shadow-2xl'>
+            
+            <div className='flex-1 flex items-center'>
+              <div className='flex flex-col w-full bg-white p-10 text-black rounded-3xl '>
+                <img src="/logo.png" alt="Logo" className='w-40 mb-1 ml-[-10px]' />
+                <h1 className='text-[#353452] text-3xl  font-semibold mb-5'>
+                  <span className='bg-gradient-to-r from-pink-400 to-rose-500 text-transparent bg-clip-text font-bold'>Teacher</span> SignUp
+                </h1>
+                <form onSubmit={handleSubmit} className='flex flex-col '>
+                  <div className='w-full flex gap-2'>
+                    <div className='flex-1 flex flex-col'>
+                      <label htmlFor="name" className='text-sm text-[#353452] ms-1'>Name</label>
+                      <input
+                        type='text'
+                        name='name'
+                        placeholder='Enter Name...'
+                        onChange={(e) => setName(e.target.value)}
+                        value={name}
+                        className='py-3 px-2  rounded-md bg-purple-100 border-4 border-white'
+                        required
+                      />
+                    </div>
+                    <div className='flex-1 flex flex-col'>
+                      <label htmlFor="experience" className='text-sm text-[#353452] ms-1'>Experience</label>
+                      <input
+                        type='number'
+                        name='experience'
+                        placeholder='Years'
+                        onChange={(e) => setExperience(e.target.value)}
+                        value={experience}
+                        className='py-3 px-2  rounded-md bg-purple-100  border-4 border-white'
+                        required
+                      />
+                    </div>
+                  </div>
+
+                  <label htmlFor="email" className='text-sm text-[#353452] ms-1 mt-2'>Email</label>
+                  <input
+                    type='email'
+                    name='email'
+                    placeholder='Enter Email...'
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                    className='py-3 px-2  rounded-md bg-purple-100 border-4 border-white'
+                    required
+                  />
+                  <div className='w-full flex gap-2 mt-2'>
+                    <div className='flex-1 flex flex-col'>
+                      <label htmlFor="subject" className='text-sm text-[#353452] ms-1'>Subject</label>
+                      <input
+                        type='text'
+                        name='subject'
+                        placeholder='Enter Subject...'
+                        onChange={(e) => setSubject(e.target.value)}
+                        value={subject}
+                        className='py-3 px-2  rounded-md bg-purple-100  border-4 border-white'
+                        required
+                      />
+                    </div>
+                    <div className='flex-1 flex flex-col'>
+                      <label htmlFor="location" className='text-sm text-[#353452] ms-1'>Location</label>
+                      <input
+                        type='text'
+                        name='location'
+                        placeholder='Enter Location...'
+                        onChange={(e) => setLocation(e.target.value)}
+                        value={location}
+                        className='py-3 px-2  rounded-md bg-purple-100  border-4 border-white'
+                        required
+                      />
+                    </div>
+                  </div>
+                  <label htmlFor="password" className='text-sm text-[#353452] ms-1 mt-3' >Password</label>
+                  <input
+                    type='password'
+                    name='password'
+                    placeholder='Enter Password...'
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                    className='py-3 px-2  rounded-md bg-purple-100 border-4 mb-5 border-white'
+                    required
+                  />
+                  
+                  <button type='submit' className='bg-gradient-to-r text-xl text-white font-bold from-fuchsia-400 to-purple-500  py-2 rounded-md  duration-150'>
+                    Submit
+                  </button>
+                </form>
+                <p className='mt-3 text-gray-600'>
+                  Already registered? <Link to='/teacher-login' className='text-blue-500 underline'>Login</Link>
+                </p>
+              </div>
+            </div>
+            <div className='flex-1 max-w-[550px] object-cover max-md:hidden p-5 pl-0' >
+              <img src="./src/assets/images/register.png" alt="Login Banner" />
+            </div>
+          </div>
+          
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 };
 
