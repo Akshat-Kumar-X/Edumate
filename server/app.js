3cd2275cb397625b42d03aee0ad7d29ecc7c9828
Akshat Kumar <akshatkumar10203@gmail.com>
2024-07-01 17:07:25 +0530

    Changes in server

--- server/app.js
+++ server/app.js
@@ -12,7 +12,10 @@ dotenv.config();
 
 const app = express();
 app.use(express.json({ limit: '50mb' }));
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173'
+}));
+
 
 // Connect to MongoDB
 await mongoose.connect(process.env.MONGODB_URI)
