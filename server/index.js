e4dd4fdfc79950311caf645a9a3ecc7724fe7c9f
Akshat Kumar <akshatkumar10203@gmail.com>
2024-07-02 01:36:56 +0530

    fixing Issues

--- server/index.js
+++ server/index.js
@@ -12,11 +12,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json({ limit: '50mb' }));
-app.use(cors({
-  origin: 'https://edumate-tutor.vercel.app',
-  methods: ['POST', 'GET', 'PUT', 'DELETE'],
-  credentials: true
-}));
+app.use(cors());
 
 
 // Connect to MongoDB
