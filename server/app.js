680abf5a5c50ab23f93fee510cb2926074e5d839
Akshat Kumar <akshatkumar10203@gmail.com>
2024-07-01 17:14:14 +0530

    Changes in server

--- server/app.js
+++ server/app.js
@@ -14,8 +14,8 @@ const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(cors({
   origin: 'http://localhost:5173',
-  methods: ['GET', 'POST'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  methods: ['POST', 'GET'],
+  credentials: true
 }));
 
 
