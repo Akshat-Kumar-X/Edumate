5778f3c54d6ed6f16afce4199c89cffc61e7e010
Akshat Kumar <akshatkumar10203@gmail.com>
2024-07-07 14:07:05 +0530

    minor changes

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ dotenv.config();
 const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://edumate-tutor.vercel.app',
   methods: ['POST', 'GET', 'PUT', 'DELETE'],
   credentials: true
 }));
